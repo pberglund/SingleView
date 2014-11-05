73c501897612a414f1285d6099729a97d63340de
pberglund <pberglund@sagepath.com>
2014-11-05 10:03:10 -0500

    Initial Commit

---
+++ SingleView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SingleView
+//
+//  Created by Sage Path on 11/5/14.
+//  Copyright (c) 2014 Sage Path. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
